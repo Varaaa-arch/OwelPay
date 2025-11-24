17667fcebc6d6a0174db7700f66f6786d5af49dd
AmandaHuxley- <amandarizkyy88@gmail.com>
2025-11-24 13:51:37 +0700

    Debugging Home Page

--- app/src/main/java/com/example/owelpay/HomePage.kt
+++ app/src/main/java/com/example/owelpay/HomePage.kt
@@ -7,17 +7,22 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class homePage : AppCompatActivity() {
+class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home_page)
 
-        val mainLayout = findViewById<ConstraintLayout>(R.id.homePage)
+        val mainLayout = findViewById<ConstraintLayout>(R.id.home_page)
+
         ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            return@setOnApplyWindowInsetsListener insets
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+            insets
         }
     }
 }
