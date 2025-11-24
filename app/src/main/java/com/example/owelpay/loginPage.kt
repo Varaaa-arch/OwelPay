f628fd74a400e19c30851bec1b06b3218a1f5879
AmandaHuxley- <amandarizkyy88@gmail.com>
2025-11-24 14:48:26 +0700

    Login Page + lupa sandi page

---
+++ app/src/main/java/com/example/owelpay/loginPage.kt
@@ -0,0 +1,32 @@
+package com.example.owelpay
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class loginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_page)
+
+        val mainLayout = findViewById<ConstraintLayout>(R.id.login)
+        ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val forgotPass = findViewById<TextView>(R.id.lupa_sandi)
+        forgotPass.setOnClickListener {
+            val intent = Intent(this, loginPage::class.java)
+            startActivity(intent)
+        }
+    }
+}
